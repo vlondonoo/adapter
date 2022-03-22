211423cea36fbeb92c4bea8a08a3e5b099b6122c
vlondonoo <vlondonoo@gmail.com>
2022-03-22 17:29:42 -0500

    feat: dockerfile

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const dotenv = require('dotenv').config();
 
 module.exports = {
-    PORT: process.env.PORT || 3000
+    PORT: process.env.PORT || 3002
 }
--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require('express');
+var cors = require('cors')
 const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
+app.use(cors({
+    origin: '*',
+    'Access-Control-Allow-Headers': '*',
+    'Access-Control-Allow-Origin': '*'
+}))
 
 const request = require('request');
 const config = require('./config.js');
 
 
-app.get('/:id', (req, res) => {
-    res.send('Get Citizen!')
-    request(`https://govcarpetaapp.mybluemix.net/apis/validateCitizen/${req.params.id}`, { json: true }, (err, res, body) => {
+app.get('/:id', cors(), (req, res) => {
+    res.send({ "response": "12345" })
+    return request(`https://govcarpetaapp.mybluemix.net/apis/validateCitizen/${req.params.id}`, { json: true }, (err, res, body) => {
         if (err) { return console.log(err); }
         console.log(body);
     });
@@ -17,7 +23,7 @@ app.get('/:id', (req, res) => {
 
 
 app.get('/:id/:urlDocument/:documentTitle', (req, res) => {
-    res.send('Result!')
+    res.send({ "response": `${req.params.documentTitle}` })
     request(`https://govcarpetaapp.mybluemix.net/apis/authenticateDocument/${req.params.id}/${req.params.urlDocument}/${req.params.documentTitle}`, { json: true }, (err, res, body) => {
         if (err) { return console.log(err); }
         console.log(body);
@@ -37,7 +43,7 @@ app.post('/registerCitizen', (req, res) => {
         if (err) { return console.log(err); }
         console.log(body);
     });
-    res.send('Response!')
+    res.send({ "response": "resp" })
 })
 
 app.listen(config.PORT, () => console.log('Server started'));
